f1fd19c28c9bb2c9f91c92bede20f029f663eb3e
igorr <igorrelic@gmail.com>
2024-12-27 20:44:14 +0100

    izmene u korpi

--- Vezba1.py
+++ Vezba1.py
@@ -1,5 +1,9 @@
-print ("Hello you too 2")
-print ("nove izmene na developu... dev grana")
+print("Ubacite nesto u korpu")
+naziv_proizvoda = input("unesite naziv:")
+cena = input ("unesite cenu:")
+print("nove izmene na developu... dev grana")
 
-print ("dodajem nove izmene")
-print ("Novi red")
+#print ("dodajem nove izmene")
+#print ("Novi red")
+
+print ("uspesno ste uneli:", naziv_proizvoda, cena)

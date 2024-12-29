6be460928394be6845e8e1a3847433f4b0b2b21c
igorr <igorrelic@gmail.com>
2024-12-29 12:41:40 +0100

    poslednja promena pred slanje

--- Vezba1.py
+++ Vezba1.py
@@ -1,9 +1,2 @@
-print("Ubacite nesto u korpu")
-naziv_proizvoda = input("unesite naziv:")
-cena = input ("unesite cenu:")
-print("nove izmene na developu... dev grana")
-
-#print ("dodajem nove izmene")
-#print ("Novi red")
-
-print ("uspesno ste uneli:", naziv_proizvoda, cena)
+for x in range (0, 36):
+    print ("Hello world")
